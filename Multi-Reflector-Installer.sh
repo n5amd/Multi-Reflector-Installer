6c0ba407991cdc897e8574b5bce4a3b59a93d5fe
Ben <43531228+n5amd@users.noreply.github.com>
2019-08-15 15:05:39 -0500

    Update Multi-Reflector-Installer.sh
    updated dash url in config.

--- Multi-Reflector-Installer.sh
+++ Multi-Reflector-Installer.sh
@@ -138,7 +138,7 @@ XLXCONFIG=/var/www/xlxd/pgs/config.inc.php
 sed -i "s/your_email/$EMAIL/g" $XLXCONFIG
 sed -i "s/LX1IQ/$CALLSIGN/g" $XLXCONFIG
 sed -i "s/\/tmp\/callinghome.php/\/xlxd\/callinghome.php/g" $XLXCONFIG
-sed -i "s/http:\/\/your_dashboard/$XLXDOMAIN/g" $XLXCONFIG
+sed -i "s/http:\/\/your_dashboard/http:\/\/$XLXDOMAIN/g" $XLXCONFIG
 chown -R www-data:www-data /xlxd/
 chown -R www-data:www-data /var/www/xlxd/
 echo "--------------------------------------"
